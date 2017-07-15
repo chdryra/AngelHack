226e07edd06fe128f49fc442e4bb496ded585b9d
starfighter-ix <ec14370@qmul.ac.uk>
2017-07-15 15:16:12 +0100

    Getting database instance from Firebase.

--- app/src/main/java/io/angelhack/verd/firebase/SampleFirebaseSDK.java
+++ app/src/main/java/io/angelhack/verd/firebase/SampleFirebaseSDK.java
@@ -1,11 +1,20 @@
 package io.angelhack.verd.firebase;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by sameenislam on 15/07/2017.
  */
 
 public class SampleFirebaseSDK {
 
+    public SampleFirebaseSDK() {
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello Verd");
+    }
 
 }
