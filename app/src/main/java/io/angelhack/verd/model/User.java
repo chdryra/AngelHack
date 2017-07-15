d9d847d317a88b71c254749b78dda2a3ed27a055
starfighter-ix <ec14370@qmul.ac.uk>
2017-07-15 17:34:53 +0100

    Adding user class.

--- app/src/main/java/io/angelhack/verd/model/User.java
+++ app/src/main/java/io/angelhack/verd/model/User.java
@@ -2,6 +2,7 @@ package io.angelhack.verd.model;
 
 import android.graphics.Bitmap;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -10,12 +11,60 @@ import java.util.UUID;
 
 public class User {
 
-    private UUID name;
+    private UUID id;
     private String name;
     private Bitmap photo;
-    private List<>
-
+    private List<UUID> following;
+    private List<UUID> followers;
 
     public User() {
+    }
+
+    public User(UUID id, String name1, Bitmap photo, List<UUID> following, List<UUID> followers) {
+        this.id = id;
+        this.name = name1;
+        this.photo = photo;
+        this.following = following;
+        this.followers = followers;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+
+    public List<UUID> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<UUID> following) {
+        this.following = following;
+    }
+
+    public List<UUID> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<UUID> followers) {
+        this.followers = followers;
     }
 }
